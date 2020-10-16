9c8fbd1c932d430977cce9c06adb32f05ebf214d
Li Tan <tanli.stonybrook@gmail.com>
2020-10-15 20:11:53 -0700

    work on android to solve the problem of dual launch

---
+++ src/android/com/missiveapp/openwith/IntentActivity.java
@@ -0,0 +1,174 @@
+package com.missiveapp.openwith;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.ClipData;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.adobe.phonegap.push.FCMService;
+import com.adobe.phonegap.push.PushPlugin;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class IntentActivity extends Activity {
+  private static String LOG_TAG = "Shared_IntentActivity";
+
+  /*
+   * this activity will be started if the user touches a notification that we own.
+   * We send it's data off to the push plugin for processing.
+   * If needed, we boot up the main activity to kickstart the application.
+   * @see android.app.Activity#onCreate(android.os.Bundle)
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState)  {
+
+    Intent intent = getIntent();
+
+    super.onCreate(savedInstanceState);
+    Log.v(LOG_TAG, "onCreate");
+
+    String callback = getIntent().getExtras().getString("callback");
+    Log.d(LOG_TAG, "callback = " + callback);
+    boolean foreground = getIntent().getExtras().getBoolean("foreground", true);
+//    boolean startOnBackground = getIntent().getExtras().getBoolean(START_IN_BACKGROUND, false);
+//    boolean dismissed = getIntent().getExtras().getBoolean(DISMISSED, false);
+//    Log.d(LOG_TAG, "dismissed = " + dismissed);
+
+//    if(!startOnBackground){
+//      NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//      notificationManager.cancel(FCMService.getAppName(this), notId);
+//    }
+//
+    boolean isPushPluginActive = PushPlugin.isActive();
+//    boolean inline = processPushBundle(isPushPluginActive, intent);
+
+//    if(inline && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.N && !startOnBackground){
+//      foreground = true;
+//    }
+//
+//    Log.d(LOG_TAG, "bringToForeground = " + foreground);
+
+    finish();
+
+
+//    if (isPushPluginActive) {
+      forceMainActivityReload(false);
+      if (intent.getAction().equals(Intent.ACTION_SEND)){
+        //this is an sending activity. We need to process this.
+        ContentResolver resolver = this.getApplicationContext().getContentResolver();
+        try {
+          JSONObject obj = Serializer.toJSONObject(resolver, intent);
+//          Log.d(LOG_TAG, "obj:");
+//          Log.d(LOG_TAG, obj.toString());
+
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+//        startActivity(intent);
+      }
+
+//    };
+//      if (!isPushPluginActive && foreground && inline) {
+//        Log.d(LOG_TAG, "forceMainActivityReload");
+//        forceMainActivityReload(false);
+//      } else if(startOnBackground) {
+//        Log.d(LOG_TAG, "startOnBackgroundTrue");
+//        forceMainActivityReload(true);
+//      } else {
+//        Log.d(LOG_TAG, "don't want main activity");
+//      }
+  }
+
+//  /**
+//   * Takes the pushBundle extras from the intent,
+//   * and sends it through to the PushPlugin for processing.
+//   */
+//  private boolean processPushBundle(boolean isPushPluginActive, Intent intent) {
+//    Bundle extras = getIntent().getExtras();
+//    Bundle remoteInput = null;
+//
+//    if (extras != null) {
+//      Bundle originalExtras = extras.getBundle(PUSH_BUNDLE);
+//
+//      originalExtras.putBoolean(FOREGROUND, false);
+//      originalExtras.putBoolean(COLDSTART, !isPushPluginActive);
+//      originalExtras.putBoolean(DISMISSED, extras.getBoolean(DISMISSED));
+//      originalExtras.putString(ACTION_CALLBACK, extras.getString(CALLBACK));
+//      originalExtras.remove(NO_CACHE);
+//
+//      remoteInput = RemoteInput.getResultsFromIntent(intent);
+//      if (remoteInput != null) {
+//        String inputString = remoteInput.getCharSequence(INLINE_REPLY).toString();
+//        Log.d(LOG_TAG, "response: " + inputString);
+//        originalExtras.putString(INLINE_REPLY, inputString);
+//      }
+//
+//      PushPlugin.sendExtras(originalExtras);
+//    }
+//    return remoteInput == null;
+//  }
+
+  /**
+   * Forces the main activity to re-launch if it's unloaded.
+   */
+  private void forceMainActivityReload(boolean startOnBackground) {
+    PackageManager pm = getPackageManager();
+    Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
+
+    Intent originalIntent = getIntent();
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+
+//      launchIntent.putExtras(extras);
+//       ClipData data = getIntent().getClipData();
+//       launchIntent.setClipData(data);
+
+       String type = getIntent().getType();
+//       if (type!=null) {
+//         launchIntent.putExtra("internalType", type);
+////         launchIntent.setType(type);
+//       }
+      JSONObject obj = null;
+      try {
+        obj = Serializer.toJSONObject(getApplicationContext().getContentResolver(), getIntent());
+        launchIntent.putExtra("json", obj.toString());
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+//      if (obj!=null){
+//        launchIntent.putExtra("json", obj.toString());
+//      }
+
+//      Bundle originalExtras = extras.getBundle(PUSH_BUNDLE);
+////      if (originalExtras != null) {
+////        launchIntent.putExtras(originalExtras);
+////      }
+////      launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//      launchIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//
+//      launchIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+//      launchIntent.putExtra("cdvStartInBackground", startOnBackground);
+////      launchIntent.putExtra(START_IN_BACKGROUND, startOnBackground);
+//    }
+//    launchIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+      launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+      launchIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+      launchIntent.putExtra("cdvStartInBackground", false);
+
+      startActivity(launchIntent);
+    }
+  }
+  @Override
+  protected void onResume() {
+    super.onResume();
+//    final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+//    notificationManager.cancelAll();
+  }
+}
